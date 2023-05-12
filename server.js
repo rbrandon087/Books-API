12550b700d4428ebb9949b4a303ed697954bed6d
Hadeel Ammari <80487488+hadeelammari@users.noreply.github.com>
2023-05-12 14:34:46 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const booksRouter = require('./controllers/books');
+const booksRouter = require('./controller/books');
 const app = express();
-const port = 3000;
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+const PORT = process.env.PORT
 
 // Configure body-parser
 app.use(bodyParser.json());
@@ -14,7 +17,14 @@ app.get('/', (req, res) => {
 // Use the books router for the '/books' endpoint
 app.use('/books', booksRouter);
 
+// MONGOOSE
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// BOOKS 
+const booksController = require('./controller/books')
+app.use('/books', booksController)
+
 // Start the server
-app.listen(port, () => {
-  console.log(`App running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`App running on http://localhost:${PORT}`);
 });
